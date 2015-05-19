2995687b6b70ecaf9ef9de5966594ab104580303
Sean Yeh <sean.yeh@gmx.com>
2015-05-19 14:47:41 -0500

    features.py

---
+++ features.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+'''
+Write all your feature-evaluating functions in this file.
+
+# All helper functions begin with an underscore
+def _favorite_helper():
+    return 1
+
+# All other functions will be recognized as a feature function
+# Feature functions will take a music21 piece object as its argument
+def my_favorite_feature(piece):
+    return _helper()
+
+'''
